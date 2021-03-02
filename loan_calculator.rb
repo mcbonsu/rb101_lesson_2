e471b21ebc1f7e3dc34fe58c75a92f1c53079142
Mc Bonsu <mccollinsbonsu@gmail.com>
2021-03-02 19:00:48 +0000

    Added 2nd language, colour and updated loan calculator to display updates

--- loan_calculator.rb
+++ loan_calculator.rb
@@ -1,76 +1,106 @@
 # Mortgage / Car Loan Calculator
 
+require 'yaml'
+MESSAGES = YAML.load_file('languages.yml')
+
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-# check if user entry is an float
-def float?(num)
-  num == num.to_f().to_s()
+def add_newline
+  puts
 end
 
-prompt("Welcome to Mortgage Calculator!")
-prompt("-------------------------------")
+def integer?(num)
+  num =~ /^-?[1-9]+$/
+end
+
+class String
+  def red;            "\033[31m#{self}\033[0m" end
+  def green;          "\033[32m#{self}\033[0m" end
+  def magenta;        "\033[35m#{self}\033[0m" end
+end
+
+def clear_screen
+  system('clear') || system('cls')
+end
+
+language = ''
+loop do
+  prompt "Please select language. En = English, Fr = French!"
+  language = gets.chomp.downcase
+  case language
+  when "en" then break
+  when "fr" then break
+  else prompt "Wrong entry. Please try again!"
+  end
+end
+
+add_newline
+prompt MESSAGES[language]['welcome']
+add_newline
+
 
 loop do # main loop
-  years_to_float = ''
+  years = ''
   interest_float = ''
   amount_to_float = ''
 
   loop do
-    prompt("What is the loan amount? ")
+    prompt MESSAGES[language]["loan_amount"]
     amount = Kernel.gets().chomp()
     amount_to_float = amount.to_f()
 
     if amount_to_float != 0 && amount_to_float > 0 && amount_to_float != ' '
       break
     else
-      prompt("Please enter a positive number")
+      prompt MESSAGES[language]["positive_number"]
     end
   end
 
   loop do
-    prompt("What is the interest rate? ")
+    prompt MESSAGES[language]["interest"]
     prompt("(Example: 5 for 5% or 2.5 for 2.5%) ")
     interest = Kernel.gets().chomp()
-    interest_float = interest.to_f()
-
-    interest_float.class()
+    interest_float = (interest.to_f()).round(2)
 
     if interest_float != 0 && interest_float > 0 && interest_float != ''
       break
     else
-      prompt("Please enter a positive number")
+      prompt MESSAGES[language]["positive_number"]
     end
   end
 
   loop do
-    prompt("What is the loan duration (in years)? ")
-    prompt("(Example: 2 for 2 years or 2.5 for 2 and half years) ")
+    prompt MESSAGES[language]["years"]
     years = Kernel.gets().chomp()
-    years_to_float = years.to_f()
 
-    if years_to_float != 0 && years_to_float > 0 && years_to_float != ' '
+    if integer?(years)
       break
     else
-      prompt("Please enter a positive number")
+      prompt MESSAGES[language]["valid_number"]
     end
   end
 
   annual_interest_rate = interest_float / 100
   monthly_rate = annual_interest_rate / 12
-  months = years_to_float * 12
+  months = years.to_f * 12
 
   monthly_payment = amount_to_float * (monthly_rate /
                                       (1 - (1 + monthly_rate)**(-months)))
 
-  prompt("Your monthly payment is: $#{format('%.2f', monthly_payment)}")
+  total = "$#{format('%.2f', monthly_payment)}"
+  prompt MESSAGES[language]["result"] + (total).green
+  add_newline
 
-  prompt("Another calculation? (Y / N)")
+  prompt MESSAGES[language]["do_again?"]
   answer = Kernel.gets().chomp()
 
   break unless answer.downcase().start_with?('y')
+  add_newline
+  clear_screen
 end
 
-prompt("Thanks for using our Mortgage Calculator!")
-prompt("Good bye!")
+prompt MESSAGES[language]["thank_you"]
+add_newline
+prompt MESSAGES[language]["goodbye"]
